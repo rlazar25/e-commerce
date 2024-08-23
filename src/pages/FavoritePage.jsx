29f147492c5a23c35f942d73c73fbb33f4d73d04
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-08-23 14:10:27 +0200

    CardProductComponent is displayed in the favorites page

--- src/pages/FavoritePage.jsx
+++ src/pages/FavoritePage.jsx
@@ -1,7 +1,18 @@
-function FavoritePage(){
-    return(
-        <div>
-            favorite page
+import { useSelector } from "react-redux";
+import CardProductComponent from "../components/CardProductComponent";
+import { Link } from "react-router-dom";
+
+function FavoritePage() {
+
+    const { allFavorite } = useSelector(state => state.favoriteStore)
+
+
+    return (
+        <div className="container mx-auto px-8 flex flex-wrap gap-8 items-center justify-center my-[50px]">
+            {allFavorite.length > 0 ? allFavorite.map(product => {
+                return <CardProductComponent key={product.id} product={product} />
+            }) : <div className="text-center "> <h2 className="text-2xl" >No Favorite Products</h2> <p>Check our <Link className="text-mainYellow" to="/">Products</Link></p> </div>}
+
         </div>
     )
 }
