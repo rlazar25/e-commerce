37f5c9667270a4e94456059e3bed40fcd45afd23
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-08-27 17:31:31 +0200

    Again added limited loading to 16 products. By clicking on show more, 16 more products are loaded

--- src/services/productServices.js
+++ src/services/productServices.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 class productServices {
   static getAllCategoryService = () => axios.get("/products/category-list");
-  // static getAllProductsService = (loadMore) => axios.get(`products?limit=${loadMore}`);
-  static getAllProductsService = () => axios.get("/products");
+  static getAllProductsService = (loadMore) => axios.get(`products?limit=${loadMore}`);
+  // static getAllProductsService = () => axios.get("/products");
   static getSingleProductService = (id) => axios.get(`/product/${id}`);
   static getAllProductsByCategory = (category) => axios.get(`/products/category/${category}`);
 }
