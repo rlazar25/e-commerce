d4882fd81e5980c844e6d10d9de1215cebfd86b7
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-08-28 20:48:06 +0200

    Added functionality for a coupon code that reduces the price by 50% when 'your smile' is entered

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,3 +1,5 @@
+import { useRef, useState } from "react";
+// mui
 import { Table, TableContainer, TableHead, TableRow, Paper, TableCell, TableBody } from "@mui/material";
 // redux
 import { useDispatch, useSelector } from "react-redux";
@@ -8,11 +10,24 @@ import { toast, ToastContainer } from "react-toastify";
 import { Link } from "react-router-dom";
 
 function CartPage() {
-    const { cart, totalPrice } = useSelector(state => state.cartStore)
-    const dispatch = useDispatch()
 
+    const [cuponCode, setCuponCode] = useState("");
+    const cuponCodeRef = useRef();
+
+    // redux
+    const { cart, totalPrice } = useSelector(state => state.cartStore);
+    const dispatch = useDispatch();
+
+    // Toastify
     const warningMsg = () => toast.warning("Out of stock", { autoClose: 1000, position: "bottom-right", theme: "colored" });
     const deleteMsg = () => toast.error("Item Removed", { autoClose: 1000, position: "bottom-right", theme: "colored" });
+
+    // handler
+    function handleCuponCode() {
+        setCuponCode(cuponCodeRef.current.value);
+        cuponCodeRef.current.value = "";
+    }
+
     return (
         <div className='mt-[50px]'>
             {cart.length > 0 ? <div className='container p-8 mx-auto flex flex-col lg:flex-row gap-[20px]'>
@@ -38,20 +53,20 @@ function CartPage() {
                                         <TableCell component="th" scope="row">
                                             <Link to={`/singleProduct/${product.id}`} className="flex gap-2 items-center">
                                                 <img src={product.thumbnail} alt="" className='w-[90px] h-[90px] border border-mainBlue rounded-lg object-cover ' />
-                                                <p className="hidden md:inline text-xl font-semibold text-mainBlue">{product.title}</p>
+                                                <p title={product.title}  className="hidden md:inline text-xl font-semibold text-mainBlue truncate ">{product.title}</p>
                                             </Link>
                                         </TableCell>
                                         <TableCell align="center">${product.price}</TableCell>
                                         <TableCell align="center">
                                             <div className="flex justify-center items-center text-[18px] font-medium">
-                                                <span onClick={() => dispatch(decreaseQuantityProduct(product))} className="cursor-pointer border border-slate-300 w-9 h-8 text-center bg-slate-100">-</span>
-                                                <p className="w-16 text-center border border-slate-300 h-8 bg-slate-100">{product.quantity}</p>
-                                                <span onClick={() => {
+                                                <button onClick={() => dispatch(decreaseQuantityProduct(product))} className="cursor-pointer border border-slate-300 w-9 h-8 text-center bg-slate-100">-</button>
+                                                <span className="w-16 text-center border border-slate-300 h-8 bg-slate-100">{product.quantity}</span>
+                                                <button onClick={() => {
                                                     dispatch(increaseQuantityProduct(product))
                                                     if (product.quantity >= product.stock) {
                                                         warningMsg()
                                                     }
-                                                }} className="cursor-pointer border border-slate-300 w-9 h-8 text-center bg-slate-100">+</span>
+                                                }} className="cursor-pointer border border-slate-300 w-9 h-8 text-center bg-slate-100">+</button>
                                             </div>
                                         </TableCell>
                                         <TableCell align="center">${(product.totalProductPrice * product.quantity).toFixed(2)}</TableCell>
@@ -75,16 +90,19 @@ function CartPage() {
                 <div className='w-full lg:w-[30%] self-start border rounded-lg '>
                     <h2 className="bg-mainBlue text-center text-white p-4 rounded-t-md">CART TOTAL</h2>
                     <div className="px-8 py-3">
-                        <div className="flex justify-between border-b py-5 border-slate-700">
+                        <div className="flex justify-between border-b py-5 border-slate-700 text-[20px] font-semibold">
                             <p>Subtotal: </p>
-                            <p>${totalPrice.toFixed(2)}</p>
+                            <p className={cuponCode === 'your smile' ? 'line-through' : ''}>${totalPrice.toFixed(2)}</p>
                         </div>
                         <div className="flex flex-col gap-4 border-b py-5 border-slate-700">
-                            <input className="border placeholder:text-slate-700 border-slate-300 p-2 w-full rounded-lg" type="text" placeholder="Enter Coupon Code" />
-                            <button className="bg-mainBlue hover:bg-mainYellow  duration-500 text-white py-2 w-full rounded-lg ">Apply Coupon Code</button>
+                            <input ref={cuponCodeRef} className="border placeholder:text-slate-700 border-slate-300 p-2 w-full rounded-lg" type="text" placeholder="Enter Coupon Code" />
+                            <button onClick={handleCuponCode} className={cuponCode === 'your smile' ? "bg-slate-400 line-through duration-500 text-black py-2 w-full rounded-lg " : "bg-mainBlue hover:bg-mainYellow  duration-500 text-white py-2 w-full rounded-lg "}> {cuponCode === 'your smile' ? 'Cupon Applied' : 'Apply Cupon Code'}</button> 
                         </div>
-                        <div className=" py-5">
-
+                        <div className="flex flex-col gap-4 py-5">
+                            <div className="flex justify-between text-[20px] font-semibold">
+                                <p>Total Amount: </p>
+                                <p>${cuponCode === 'your smile' ? (totalPrice / 2).toFixed(2) : totalPrice.toFixed(2)}</p>
+                            </div>
                             <button className="bg-mainBlue hover:bg-mainYellow  duration-500 text-white py-2 w-full rounded-lg self-start">Checkout</button>
                         </div>
                     </div>
